5354b40888eff1d4e715c3a514d1f80288d50769
suatozcan <email@example.com>
2020-09-02 18:03:47 +0300

    Add WebException try and catch block. Display WebException Status.

---
+++ ErrorHandlingOverTheWire/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ErrorHandling;
+using System.Net;
+using System.IO;
+
+namespace ErrorHandlingOverTheWire
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            ResultObject result = new ResultObject();
+            WebRequest request = WebRequest.Create("http://test-domain.com");
+            request.Method = "POST";
+            Stream reqStream = request.GetRequestStream();
+            using (StreamWriter sw = new StreamWriter(reqStream))
+            {
+                sw.Write("Our test data query");
+            }
+            var responseTask = request.GetResponseAsync();
+           
+            try
+            {
+
+                var webResponse = await responseTask;
+                using (StreamReader sr = new
+               StreamReader(webResponse.GetResponseStream()))
+                {
+                    result.RequestResult = await sr.ReadToEndAsync();
+                }
+            }
+            catch (WebException ex)
+            {
+
+                Console.WriteLine(ex.Status);
+                Console.WriteLine(ex.Message);
+            }
+
+           
+        }
+    }    
+}
